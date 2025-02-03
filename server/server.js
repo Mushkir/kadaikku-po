12fce25e86b05c1ec643f3c599d1e38113a2a58f
Mushkir <mushkirmohamed@gmail.com>
2025-02-03 13:25:57 +0530

    `Added cors dependency and enabled CORS in server.js`

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import itemsRouter from "./routes/index.routes.js";
 import connectDB from "./config/db.js";
@@ -6,7 +7,15 @@ import connectDB from "./config/db.js";
 const app = express();
 const PORT = 8080;
 
-app.use(express.json());
+// const corsOptions = {
+//   credentials: true,
+//   origin: "http://localhost:5173",
+// };
+
+// app.use(cors(corsOptions));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use("/", itemsRouter);
 
