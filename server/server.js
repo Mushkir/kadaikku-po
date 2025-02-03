eb44d68af44aa2854d1826a91944dcf52c4be400
Mushkir <mushkirmohamed@gmail.com>
2025-02-03 13:32:42 +0530

    `Update CORS origin in server.js to http://192.168.1.7:8081`

--- server/server.js
+++ server/server.js
@@ -7,13 +7,13 @@ import connectDB from "./config/db.js";
 const app = express();
 const PORT = 8080;
 
-// const corsOptions = {
-//   credentials: true,
-//   origin: "http://localhost:5173",
-// };
+const corsOptions = {
+  credentials: true,
+  origin: "http://192.168.1.7:8081",
+};
 
-// app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors(corsOptions));
+// app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
