25dbe57151dfe1d6e9edd0c997641f83f7ba01a6
Mushkir <mushkirmohamed@gmail.com>
2025-02-04 00:08:11 +0530

    `Refactor db connection logic in db.js`

--- server/config/db.js
+++ server/config/db.js
@@ -2,14 +2,17 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-
 const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) return;
+
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("MongoDB Connected...");
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Database connection error", err);
   }
 };
 
