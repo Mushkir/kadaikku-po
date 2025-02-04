d5c96bc329b3406184763f1e31674082e771edb8
Mushkir <mushkirmohamed@gmail.com>
2025-02-05 02:32:33 +0530

    `Refactor API URI and dependencies in server and client projects`

--- client/app/context/AppContext.tsx
+++ client/app/context/AppContext.tsx
@@ -1,5 +1,4 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
-import { API_URI } from "../../API_URI";
 
 interface AppContextType {
   listItems: any[];
@@ -13,19 +12,10 @@ export const AppProvider = ({ children }: { children: ReactNode }) => {
 
   const getAllLists = async () => {
     try {
-      // const response = await fetch(
-      //   API_URI + "/get-all-lists",
-      //   {
-      //     credentials: "include",
-      //     method: "GET",
-      //   }
-      const response = await fetch(
-        "https://kadaikku-po.vercel.app/get-all-lists",
-        {
-          credentials: "include",
-          method: "GET",
-        }
-      );
+      const response = await fetch(``, {
+        credentials: "include",
+        method: "GET",
+      });
 
       const respData = await response.json();
       console.log(respData);
